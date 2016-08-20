9d7f955283e3c8080e72530661685db7e37acf8e
kosamari <mariko.kosaka@gmail.com>
2016-08-19 22:21:23 -0400

    string

--- hello.c
+++ hello.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /*
-Arrays
+String
+
+string s; => doesn't exist in C
+in C string is Array of char type (always end with \0)
+
+These 3 ways do the same thing
+char s[] = {'a', 'b', 'c', '\0'};
+char s[] = "abc";
+char s[4] = "abc"; if you specify length of an array, don't forget there is '\0' at the end
 */
 
 int main(void) {
-  int sales[3]; /* specify size */
-  sales[0] = 200;
-  sales[1] = 400;
-  sales[2] = 300;
-
-  /* same thing as above, you can omit size of array in [] */
-  int sales2[] = {200, 400, 300};
-
-  printf("%d\n", sales[1]);
-  printf("%d\n", sales2[1]);
+  char s[] = "abc";
+  printf("%c\n", s[1]); /* b */
   return 0;
 }
