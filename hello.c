2dcbcea806b5affd42260e4a0fd96aa35b9c7f16
kosamari <mariko.kosaka@gmail.com>
2016-08-19 21:26:16 -0400

    variables

--- hello.c
+++ hello.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-/* commnt */
-
+/*
+variable - label for data
+Data Types (famous ones)
+  - int(integers) %d
+  - float(decimals) %f
+  - char(one character) %c
+*/
 int main(void) {
-  printf("hello world\n");
+
+  int x; /* make variable */
+  x = 10; /* assign data to variable */
+  float y = 5.2;
+  char c = 'A';
+
+  printf("x = %d, y = %f. c = %c\n", x, y, c);
   return 0;
 }
