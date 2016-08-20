1234dd9be6645d693b05d161204df807bc6351d0
kosamari <mariko.kosaka@gmail.com>
2016-08-19 21:58:29 -0400

    function cont. void & prototype declaration

--- hello.c
+++ hello.c
@@ -12,7 +12,27 @@ how to write function:
 
 how call function:
 [function name]([argument], ...)
+
+If function don't take arguments nor return value, use `void`
+In C - main function is the one it is always called.
 */
+
+/* Prototype Declaration - when you write function after main function */
+float getMax(float a, float b);
+
+void sayHi(void) {
+  printf("hi!\n");
+}
+
+int main(void) {
+  float result;
+  result = getMax(2.3, 5.2);
+  printf("%f\n", result);
+
+  sayHi();
+
+  return 0;
+}
 
 float getMax(float a, float b) {
   if (a >= b) {
@@ -20,11 +40,4 @@ float getMax(float a, float b) {
   } else {
     return b;
   }
-}
-
-int main(void) {
-  float result;
-  result = getMax(2.3, 5.2);
-  printf("%f\n", result);
-  return 0;
 }
