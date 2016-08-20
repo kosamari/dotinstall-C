aa499190dd22dde602358d3cd81fc1b26047abda
kosamari <mariko.kosaka@gmail.com>
2016-08-19 22:44:36 -0400

    Save memory with pointer 2

--- hello.c
+++ hello.c
@@ -8,15 +8,18 @@ large integer : long() %ld
 takes 8 memory addresses
 
 the following code used 16 memory addresses
+
+Pointer variable only takes 4 memory addresses
 */
 
-void f(long a) {
-  a += 100; /* copy 1000 in memory address 9-16, then add 100 */
-  printf("%ld\n", a);
+void f(long *pa) { /* create pointer from memory address 9-12 */
+  *pa += 100; /* 1. access memory address 1-8 - pointed by pointer variable.
+                 2. add 100 to the value saved in address 1-8 */
+  printf("%ld\n", *pa);
 }
 
 int main(void) {
   long a = 1000; /* add 1000 in memory address 1-8 */
-  f(a);
+  f(&a); /* pass address of a to function f */
   return 0;
 }
