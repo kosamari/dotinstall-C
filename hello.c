3187586ff8c05e43d4dedef5da9184b07168a6cb
kosamari <mariko.kosaka@gmail.com>
2016-08-19 22:31:33 -0400

    Pointer cont. Address operator & Indirection operator

--- hello.c
+++ hello.c
@@ -4,10 +4,18 @@
 Pointer : variable to store address in memory
 
 int takes memory of 4 addresses
+
+use * to create pointer
+& : address operator
+* : indirection operator (get value from the address which pointer variable is pointing to)
 */
 
 int main(void) {
   int a; /* naming 4 addresses as a */
   a = 10;/* keep value 10 in the address named a */
+
+  int *pa; /* use * to create pointer */
+  pa = &a; /* point to address of variable a*/
+  printf("%d\n", *pa); /* get variable from the memory address where pointer variable is pointing to*/
   return 0;
 }
